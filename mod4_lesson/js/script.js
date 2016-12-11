0752ea3b945ee434697f92c5a0f0b473dc2ca1fd
Timothy Sayre <buckyinsfo@gmail.com>
2016-12-11 00:21:51 -0800

    Committing homework for week 4.

---
+++ mod4_lesson/js/script.js
@@ -0,0 +1,17 @@
+// function Dog(name) {
+//   this.name = name;
+// }
+
+// Dog.prototype.bark = function () {
+//   console.log(this.name + " likes barking! Bark!");
+// }
+
+// var max = new Dog("Max", "Buddy");
+// max.bark();
+
+var counter = 0;
+var myArray = ["Yaakov", 2, {handle: "yaakovchaikin"}];
+for (var i = 0; i < myArray.length; i++) {
+  counter++;
+}
+console.log(counter);
